cc25009c725aa55fa0e5a181799db00a3be2a3c7
leungxa <alex@myeatclub.com>
2016-09-01 04:09:24 -0700

    Create learner.py

---
+++ learner.py
@@ -0,0 +1,163 @@
+from collections import OrderedDict
+
+GLOBAL_SUBMISSION_POOL = []
+
+class Status:
+    DO_NOTHING, WORKING_ON_SUBMISSION, WAITING_TO_REVIEW, WORKING_ON_REVIEW, \
+    FINISH_REVIEW, WAITING_FOR_GRADE, DONE = range(7)
+
+class Learner:
+    def __init__(self, l_id, l_fsst, l_fstg, l_review_bias):
+        self.id = l_id
+        self.first_sub_start_time = l_fsst
+        self.first_sub_true_grade = l_fstg
+        self.review_bias = l_review_bias
+        self.sub_count = 0
+        self.review_count = 0
+        self.curr_status = Status.DO_NOTHING
+        self.working_on_sub = None
+        self.last_sub = None
+        self.reviewing_sub = None
+        self.review_start_time = None
+    
+    def start_submission(self, curr_tick):
+        if curr_tick != self.first_sub_start_time:
+            return
+        true_grade = min(Submission.MAX_SCORE, \
+                         max(Submission.MIN_SCORE, \
+                             self.first_sub_true_grade + self.sub_count * 15))
+        sub = Submission(self, curr_tick, true_grade)
+        self.working_on_sub = sub
+        self.curr_status = Status.WORKING_ON_SUBMISSION
+    
+    def workon_or_submit_submission(self, curr_tick):
+        # do nothing until time to submit
+        if self.working_on_sub and \
+                self.working_on_sub.sub_start_time + Submission.WORKING_TIME == curr_tick:
+            GLOBAL_SUBMISSION_POOL.append(self.working_on_sub)
+            self.last_sub = self.working_on_sub
+            del self.working_on_sub
+            self.sub_count += 1
+            self.curr_status = Status.WAITING_TO_REVIEW
+    
+    def wait_subs_to_review(self, curr_tick):
+    # waiting for submissions to review
+        # failing grade, work on next submission
+        if self.curr_sub.failing():
+            self.start_submission(curr_tick)
+        # if pool contains submissions they can review, start to review
+        sub_to_review = Submission.get_available_sub_to_review(learner)
+        if sub_to_review:
+            start_review(self, curr_tick, sub_to_review)
+        # if ^ dont apply, don't do anything
+
+    def has_grade():
+        return len(self.last_sub.scores) == Submission.REQUIRED_REVIEW_NUM
+        
+    def start_review(self, curr_tick, submission):
+        # goes through submissions, find one to review
+        self.reviewing_sub = submission
+        submission.current_reviewer_ids.append(self.id)
+        self.review_start_time = curr_tick
+        self.curr_status = Status.WORKING_ON_REVIEW
+    
+    def workon_or_submit_review(self, curr_tick):
+        # do nothing until time to submit
+        if self.review_start_time + Submission.REVIEW_TIME == curr_tick:
+            sub = self.reviewing_sub
+            score = min(Submission.MAX_SCORE, \
+                        max(Submission.MIN_SCORE, sub.sub_true_grade + self.review_bias))
+            sub.scores.append(score)
+            sub.current_reviewer_ids.remove(self.id)
+            self.review_count += 1
+            self.curr_status = Status.FINISH_REVIEW
+    
+    def after_submitting_review(self, curr_tick):
+    # finishes a review
+        # failing grade, work on next submission
+        if self.curr_sub.failing():
+            self.start_submission(curr_tick)
+        # user submit fewer reviews than 3x theirsumbmissioncount, wait for subs to review
+        if self.review_count < 3 * self.submission_count:
+            self.wait_subs_to_review(curr_tick)
+        # if learners submission does not have a grade, wait for a grade
+        if not self.has_grade():
+            self.wait_for_grade(curr_tick)
+        # if ^ dont apply, learner is finished and stops doing anything
+        self.curr_status = Status.DONE 
+    
+    def wait_for_grade(self, curr_tick):
+        if self.has_grade():
+            # failing grade, work on next submission
+            if self.curr_sub.failing():
+                self.start_submission(curr_tick)
+            else:
+                self.curr_status = Status.DONE
+    
+    def tick(curr_tick):
+        if self.curr_status == Status.DO_NOTHING:
+            self.start_submission(curr_tick)
+                
+        if self.curr_status == Status.WORKING_ON_SUBMISSION:
+            self.workon_or_submit_submission(curr_tick)
+        
+        if self.curr_status == Status.WAITING_TO_REVIEW:
+            self.wait_subs_to_review(curr_tick)
+
+        if self.curr_status == Status.WORKING_ON_REVIEW:
+            self.workon_or_submit_review(curr_tick)
+                
+        if self.curr_status == Status.FINISH_REVIEW:
+            self.after_submitting_review(curr_tick)
+        
+        if self.curr_status == Status.WAITING_FOR_GRADE:
+            self.wait_for_grade(curr_tick)
+
+
+class Submission:
+    MIN_SCORE = 0
+    MAX_SCORE = 100
+    WORKING_TIME = 50
+    REVIEW_TIME = 20
+    PASSING_SCORE = 240
+    REQUIRED_REVIEW_NUM = 3
+    
+    def __init__(self, author, sub_start_time, true_grade):
+        self.author = author
+        self.sequence_num = author.submission_count + 1
+        self.sub_start_time = sub_start_time
+        self.sub_true_grade = true_grade
+        self.grade_tick = -1
+        self.scores = []
+        self.current_reviewer_ids = set()
+
+    @staticmethod
+    def get_available_sub_to_review(learner):
+        for sub in GLOBAL_SUBMISSION_POOL:
+            if (sub.author.id != learner.id) and \
+                    (learner.id not in sub.current_reviewer_ids) and \
+                    (len(scores) + len(current_reviewer_ids) < self.REQUIRED_REVIEW_NUM):
+                return sub
+        return None
+        
+    def score_sum(self):
+        return sum(scores)
+    
+    def failing(self):
+        score = self.score_sum()
+        if (len(self.scores) == 3 and score < self.PASSING_SCORE) or \
+                (len(self.scores) == 2 and score < self.PASSING_SCORE - 100) or \
+                (len(self.scores) == 1 and score < self.PASSING_SCORE - 200):
+            return True
+        return False
+        
+def simulate(ticks):
+    if ticks <= 0:
+        return
+    
+    # create learners
+    
+    # tick learners
+    for i in range(ticks):
+        for l in learners:
+            l.tick(i)
