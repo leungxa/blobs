7a382261d3974a940404fa6dd12ae442273d8b10
leungxa <alex@myeatclub.com>
2016-09-16 12:43:29 -0700

    Update max_difference.py

--- max_difference.py
+++ max_difference.py
@@ -2,9 +2,7 @@ def maxDifference(a):
     if len(a) == 1:
         return 0
     max_diff = -1
-    i_min = 0
     v_min = a[0]
-    i_max = None
     v_max = None
     for i, num in enumerate(a):
         if i > 0:
@@ -12,17 +10,13 @@ def maxDifference(a):
                 if v_max and v_max - v_min > max_diff:
                     max_diff = v_max - v_min
                 v_min = num
-                i_min = i
-                i_max = None
                 v_max = None
             else:
                 if num >= v_min:
-                    if i_max is None and v_max is None:
-                        i_max = i
+                    if v_max is None:
                         v_max = num
                     elif num >= v_max:
-                            i_max = i
-                            v_max = num
+                        v_max = num
                     if v_max - v_min > max_diff:
                         max_diff = v_max - v_min
     
